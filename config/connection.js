717e770440d173c8f308b7c8c27d9add6033d3c8
Yvette <yvmontes@gmail.com>
2019-05-03 21:55:14 -0700

    first commit

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "webuser",
+    password: "UCR",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
